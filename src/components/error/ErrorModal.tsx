0ee5ee626ed0723d2e34017215a7b6deac046715
Médéric Delamare <mederic.delamare@trsb.net>
2024-12-04 16:54:16 +0100

    feat: test error

--- src/components/error/ErrorModal.tsx
+++ src/components/error/ErrorModal.tsx
@@ -1,12 +1,8 @@
 'use client'
 
-import Modal from '@/design-system/modals/Modal'
-import ErrorContent from './ErrorContent'
 
 export default function ErrorModal() {
   return (
-    <Modal isOpen={true} closeModal={() => {}} hasAbortButton={false}>
-      {<ErrorContent />}
-    </Modal>
+    <div>test</div>
   )
 }

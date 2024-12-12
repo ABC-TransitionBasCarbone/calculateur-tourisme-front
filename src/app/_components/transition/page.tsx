e8aa473c6d309249e765e5708a4fd9a5cbf9bcd8
mdelamare <mederic.delamare@digiwin.fr>
2024-12-12 18:59:03 +0100

    feat: Add script for transport transition

--- src/app/_components/transition/page.tsx
+++ src/app/_components/transition/page.tsx
@@ -2,30 +2,33 @@ import React, { useEffect } from 'react';
 
 export const TransitionPage = ({ transitionPage }: { transitionPage: string }) => {
   useEffect(() => {
-    /*const script = document.createElement('script');
-    script.src = "https://impactco2.fr/iframe.js";
-    script.async = true;
-    script.setAttribute('data-type', 'transport');
-    script.setAttribute('data-search', '?theme=default&language=fr&km=100&defaultMode=list');
-    script.setAttribute('name', 'impact-co2');
+    if (transitionPage === 'transport') {
+      const script = document.createElement('script');
+      script.src = "https://impactco2.fr/iframe.js";
+      script.async = true;
+      script.setAttribute('data-type', 'transport');
+      script.setAttribute('data-search', '?theme=default&language=fr&km=100&defaultMode=list');
+      script.setAttribute('name', 'impact-co2');
 
-    const target = document.getElementById('impact-co2-container');
-    if (target) {
-      target.appendChild(script);
-    } else {
-      console.error("Conteneur non trouvé");
-    }
-
-    return () => {
+      const target = document.getElementById('impact-co2-container');
       if (target) {
-        target.innerHTML = '';
+        target.appendChild(script);
+      } else {
+        console.error("Conteneur non trouvé");
       }
-    };*/
-  }, []);
+
+      return () => {
+        if (target) {
+          target.innerHTML = '';
+        }
+      };
+    }
+  }, [transitionPage]);
 
   return (
-    <div id="impact-co2-container">
-      { transitionPage }
+    <div>
+      <div>{transitionPage}</div>
+      <div id="impact-co2-container"></div>
     </div>
   );
 };

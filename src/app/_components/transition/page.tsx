cee94ff1385b93fad82e4d70d048aac6cd24fbdf
mdelamare <mederic.delamare@digiwin.fr>
2024-12-12 18:40:24 +0100

    feat: Passing transition pages after categories

--- src/app/_components/transition/page.tsx
+++ src/app/_components/transition/page.tsx
@@ -1,3 +1,31 @@
+import React, { useEffect } from 'react';
+
 export const TransitionPage = ({ transitionPage }: { transitionPage: string }) => {
-    return <div>{transitionPage}</div>
-}
+  useEffect(() => {
+    /*const script = document.createElement('script');
+    script.src = "https://impactco2.fr/iframe.js";
+    script.async = true;
+    script.setAttribute('data-type', 'transport');
+    script.setAttribute('data-search', '?theme=default&language=fr&km=100&defaultMode=list');
+    script.setAttribute('name', 'impact-co2');
+
+    const target = document.getElementById('impact-co2-container');
+    if (target) {
+      target.appendChild(script);
+    } else {
+      console.error("Conteneur non trouvé");
+    }
+
+    return () => {
+      if (target) {
+        target.innerHTML = '';
+      }
+    };*/
+  }, []);
+
+  return (
+    <div id="impact-co2-container">
+      { transitionPage }
+    </div>
+  );
+};

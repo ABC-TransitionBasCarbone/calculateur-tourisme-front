7c7373f91ddb76c6379fe656dc0cf91c07063fbe
Chloe M <chloe.molle@neuf.fr>
2025-03-26 14:55:10 +0100

    fix page accueil

--- src/app/_components/Explanations.tsx
+++ src/app/_components/Explanations.tsx
@@ -12,7 +12,7 @@ export default async function Explanations() {
 
         <p className="md:text-lg">
           <Trans>
-            Est-ce possible de concilier plaisir et séjour réussi ? Nous le pensons ! C’est pourquoi, l’Association pour la transition Bas Carbone et Haut de France Tourisme ont conçu Mon séjour durable, afin de s’appuyer sur la notion d’empreinte carbone pour sensibiliser et orienter les comportements et choix d’activités futures vers des alternatives plus durables et plus écologiques. Pensé comme un guide local, Mon séjour durable vous aidera à faire le bon choix pour que votre séjour en Haut de France soit réussi et écologique.
+            Est-ce possible de concilier plaisir et séjour réussi ? De prendre soin de soi, des autres et de la planète ? Nous le pensons ! C’est pourquoi, l’Association pour la transition Bas Carbone et Haut de France Tourisme ont conçu Mon Séjour Durable, afin de s’appuyer sur la notion d’empreinte carbone pour sensibiliser et orienter les comportements et choix d’activités futures vers des alternatives plus durables et plus écologiques. Pensé comme un guide local, Mon Séjour Durable vous aidera à faire le bon choix pour que votre séjour en Haut de France soit réussi et écologique.
           </Trans>
         </p>
       </div>
@@ -25,7 +25,7 @@ export default async function Explanations() {
           <Trans>
             <p>
               Cet outil disponible gratuitement a été développé par l’Association pour la transition Bas Carbone avec le soutien de Haut de France Tourisme.
-              Cette déclinaison s'appuie librement sur la version officielle de Nos Gestes Climat développée par l'ADEME (Agence de la transition écologique) en partenariat avec l'ABC (Association pour la Transition Bas Carbone).
+              Cette déclinaison s'appuie librement sur la version officielle de Nos Gestes Climat développée par l'ADEME (<a href="https://www.ademe.fr/">Agence de la transition écologique</a>) en partenariat avec l'ABC (<a href="https://abc-transitionbascarbone.fr/">Association pour la Transition Bas Carbone</a>).
             </p>
             <p>
               Si vous avez des suggestions d’amélioration ou des questions, faites-en nous part  !
@@ -40,29 +40,24 @@ export default async function Explanations() {
       </div>
       <div className="mx-auto mb-12 w-full max-w-3xl px-4 md:mb-24">
         <Title tag="h2" className="font-medium md:text-3xl">
-          <Trans>Mon séjour durable</Trans>
+          <Trans>Mon Séjour Durable</Trans>
         </Title>
 
         <div className="md:text-lg">
           <Trans>
             <p>
-              Développé par l’ABC et Haut de France Tourisme
+              Développé par <a href="https://abc-transitionbascarbone.fr/">l’ABC</a> et <a href="https://www.tourisme-en-hautsdefrance.com/">Haut de France Tourisme</a>
             </p>
 
             <p>
-              L’ABC, au cœur de la lutte contre le changement climatique depuis 2011, sensibilise, forme, fédère et donne des moyens d’action concrets aux organisations et aux citoyens pour réussir leur transition bas carbone.
+              <a href="https://abc-transitionbascarbone.fr/">L’ABC</a>, au cœur de la lutte contre le changement climatique depuis 2011, sensibilise, forme, fédère et donne des moyens d’action concrets aux organisations et aux citoyens pour réussir leur transition bas carbone.
             </p>
 
             <p>
-              Haut de France Tourisme, promeut le tourisme durable dans la région Haut de France
+              <a href="https://www.tourisme-en-hautsdefrance.com/">Haut de France Tourisme</a>, promeut le tourisme durable dans la région Haut de France
             </p>
           </Trans>
         </div>
-        <ButtonLink
-          color="secondary"
-          href="/">
-          <Trans>Contact</Trans>
-        </ButtonLink>
       </div>
     </>
   )

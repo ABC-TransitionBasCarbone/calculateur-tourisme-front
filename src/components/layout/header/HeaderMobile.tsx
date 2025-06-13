3512f4b12dd2eb7c63237f7129f042b50ba07db5
Chloe M <chloe.molle@neuf.fr>
2025-06-13 08:36:58 +0200

    change typos

--- src/components/layout/header/HeaderMobile.tsx
+++ src/components/layout/header/HeaderMobile.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Logo from '@/components/misc/Logo'
 import { useIframe } from '@/hooks/useIframe'
 import { twMerge } from 'tailwind-merge'
 import BottomMenu from './headerMobile/BottomMenu'
@@ -18,8 +17,6 @@ export default function HeaderMobile({ isSticky }: Props) {
         'flex justify-between bg-white p-4 shadow-sm lg:hidden',
         isSticky ? 'sticky top-0 z-50' : ''
       )}>
-      <Logo />
-
       {!isIframeOnlySimulation && (
         <>
           <FoldableMenu />

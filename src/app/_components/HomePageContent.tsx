200cd754948b3cb64abe1896cf515d59e8e54ad8
Chloe M <chloe.molle@neuf.fr>
2025-12-24 08:47:50 +0100

    remove useless mounted

--- src/app/_components/HomePageContent.tsx
+++ src/app/_components/HomePageContent.tsx
@@ -1,23 +1,14 @@
 'use client'
 
 import Main from "@/design-system/layout/Main";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import Heading from "./Heading";
 import Explanations from "./Explanations";
 import UserContext from '@/publicodes-state/providers/userProvider/context';
 
 
 export default function HomepageContent() {
-  const [mounted, setMounted] = useState(false);
   const { territory } = useContext(UserContext)
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted || !territory) {
-    return null;
-  }
 
   return (
     <>
